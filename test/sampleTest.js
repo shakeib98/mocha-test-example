b74e371b883c62d8c8d4cbfa48e00b89d873eb9d
shakeib98 <shakeib98@outlook.com>
2019-10-06 19:11:25 +0500

    axios example added

--- test/sampleTest.js
+++ test/sampleTest.js
@@ -2,36 +2,37 @@ var assert = require('assert');
 var should = require('should')
 var request = require('request')
 var expect = require('chai').expect
+var axios = require('axios')
 
 var baseUrl = 'https://www.swapi.co/api'
 
 
 /*** basic tests ***/
-describe('Sample tests', function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
+describe('Sample tests', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
             assert.equal([1, 2, 3].indexOf(4), -1);
         });
     });
-    describe('Equals to',()=>{
-        it('it should return true to 1 equals to 1',()=>{
-            assert.equal(1===1,true)
+    describe('Equals to', () => {
+        it('it should return true to 1 equals to 1', () => {
+            assert.equal(1 === 1, true)
         })
-        it('it should return false to 1 === 2',()=>{
-            assert.equal(1===2,false)
+        it('it should return false to 1 === 2', () => {
+            assert.equal(1 === 2, false)
         })
     })
 });
 
 
 /*** api tests ***/
-describe('returns luke', function() {
-    it('returns luke', function(done) {
-        request.get({ url: baseUrl + '/people/1/' },
-            (error, response, body)=> {
+describe('returns luke', function () {
+    it('returns luke', function (done) {
+        request.get({url: baseUrl + '/people/1/'},
+            (error, response, body) => {
                 const bodyObj = JSON.parse(body);
                 /*** with assert ***/
-                assert(bodyObj.name === "Luke Skywalker",true)
+                assert(bodyObj.name === "Luke Skywalker", true)
 
                 /*** with expect ***/
                 // expect(bodyObj.name).to.equal("Luke Skywalker");
@@ -42,7 +43,7 @@ describe('returns luke', function() {
     });
 });
 
-describe('custom api',()=>{
+describe('custom api without axios', () => {
     it('it should return 200',(done)=>{
         request.get({url:'http://localhost:3000/getUsers'},(err,res,body)=>{
 
@@ -80,5 +81,27 @@ describe('custom api',()=>{
             done()
         })
 
+    })
+})
+
+describe('custom api with axios',()=>{
+    it('get request', (done) => {
+        axios.get('http://localhost:3000/getUsers').then(res => {
+            assert(res.status ===200, true)
+            done()
+        }).catch(err=>{
+            // done()
+            console.log(err)
+        })
+    })
+
+    it('post request', (done) => {
+        axios.post('http://localhost:3000/postUsers', {user: "HI"}).then(res => {
+            assert(res.status === 200,true)
+            done()
+        }).catch(err=>{
+            console.log(err)
+            // done()
+        })
     })
 })
